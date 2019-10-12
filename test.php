a4e6c0fafd1677ee60234dceb4406818e865fdcf
sorry510 <sorry510sf@gmail.com>
2019-10-12 09:51:52 +0800

    fix:修改执行方式

--- test.php
+++ test.php
@@ -3,10 +3,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use PHPZxing\PHPZxingDecoder;
 
-function pdf2png($pdf, $path) {
+function pdf2png($pdf, $path='') {
 	$pdf = str_replace('\\', '/', $pdf);
+    if(empty($path)) {
+        $path = __DIR__ . '/imgs/';
+    }
 	$path = str_replace('\\', '/', $path);
-	$filename = $path . "/" . md5(time()) . '.png';
+	$filename = $path . pathinfo($pdf)['filename'] . '.png';
 	$cmd = "gswin64 -dQUIET -dNOSAFER -r300 -dBATCH -sDEVICE=pngalpha -dNOPAUSE -sOutputFile={$filename} {$pdf}";
     shell_exec($cmd);
     return $filename;
@@ -16,28 +19,30 @@ function readQrcode($path) {
     $config = array(
         'try_harder' => true,
     );
-    // window
     $decoder = new PHPZxingDecoder($config);
     // $decoder->setJavaPath('E:\\env\\Java\\jdk1.8.0_171\\jre\\bin\\java.exe');
-    $decoder->setJavaPath('java');
+    $decoder->setJavaPath('java'); // 全局环境变量 global env
     $path = str_replace('\\', '/', $path);
-    $decodedData = $decoder->decode('file:///' . $path);
+    substr(PHP_OS, 0, 3) === 'WIN' && $path = 'file:///' . $path; // window is must write file:///
+    $decodedData = $decoder->decode($path);
     if($decodedData->isFound()) {
 		return $decodedData->getImageValue();
-	}
+	}else {
+        return 'not find qrcode';
+    }
 }
-// $decoder = new \PHPZxing\PHPZxingDecoder();
-// // Set java path with double quote '"path/to/java/exe"'
-// $decoder->setJavaPath('"C:\Program Files\Java\jdk1.8.0_202\bin\java.exe"');
-// // replace \ by / in file path
 
-// // add file:/// before file Path
-// $data = $decoder->decode('file:///'.$newPath);
-$pdf = './test.pdf';
-$path = 'E:\php\qrcode';
-$img = pdf2png($pdf, $path);
-$code = readQrcode($img);
-echo $code;
-// $path = 'C:/Users/admin/Desktop/test.png';
-// $path = 'C:/Users/admin/Desktop/test.pdf';
-// echo readQrcode($path);
+(function() {
+    $args = func_get_args();
+    if(count($args) > 1) {
+        $pdf = $args[1];
+        $path = isset($args[2]) ? $args[2] : '';
+        $img = pdf2png($pdf, $path);
+        $code = readQrcode($img);
+        echo $code;
+    }else {
+        echo 'example1: php test.php inputFile';
+        echo PHP_EOL;
+        echo 'example2: php test.php inputFile outputFile(is must absulute path)';
+    }
+})(...$argv);
